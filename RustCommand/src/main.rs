ea761015ca1037d8a85165491344df96222662c8
Reid Luttrell <lut@pdx.edu>
2023-11-17 12:14:22 -0800

    proof of concept of missiles

--- RustCommand/src/main.rs
+++ RustCommand/src/main.rs
@@ -5,12 +5,18 @@ use ggez::graphics::{self, Color};
 use ggez::input::keyboard::KeyCode;
 use ggez::timer;
 use ggez::{Context, ContextBuilder, GameResult};
-
 use ggez::input::keyboard::KeyInput;
+use oorandom::Rand32;
 use std::env;
 use std::path;
 
 type Point2 = Vec2;
+
+fn vec_from_angle(angle: f32) -> Vec2 {
+    let vx = angle.sin();
+    let vy = angle.cos();
+    Vec2::new(vx, vy)
+}
 
 fn world_to_screen_coords(screen_width: f32, screen_height: f32, point: Point2) -> Point2 {
     let x = point.x + screen_width / 2.0;
@@ -36,13 +42,57 @@ impl Default for InputState {
 }
 
 #[derive(Debug)]
+enum ActorType {
+    Cursor,
+    Rocket,
+}
+
+#[derive(Debug)]
 struct Actor {
+    tag: ActorType,
     pos: Point2,
+    angle: f32,
 }
 
 const CURSOR_VEL: f32 = 200.0;
 const CURSOR_WIDTH: f32 = 40.0;
 const CURSOR_HEIGHT: f32 = 15.0;
+
+const ROCKET_WIDTH: f32 = 15.0;
+const ROCKET_HEIGHT: f32 = 15.0;
+
+fn create_player_cursor() -> Actor {
+    Actor {
+        tag: ActorType::Cursor,
+        pos: Point2::ZERO,
+        angle: 0.0,
+    }
+}
+
+fn create_rocket() -> Actor {
+    Actor {
+        tag: ActorType::Rocket,
+        pos: Point2::ZERO,
+        angle: 0.0,
+    }
+}
+
+fn create_rockets(rng: &mut Rand32, num: i32, x: f32, y: f32) -> Vec<Actor> {
+    let screen_x = x / 2.0;
+    let screen_y = y / 2.0;
+
+    let new_rocket = |_| {
+        let mut rocket = create_rocket();
+        let start_pos = Vec2::new(rng.rand_float() * x - screen_x, screen_y);
+        let angle = rng.rand_float() * 0.5 * std::f32::consts::PI + 0.75 * std::f32::consts::PI;
+        rocket.pos = start_pos;
+        rocket.angle = angle;
+        rocket
+    };
+    (0..num).map(new_rocket).collect()
+}
+
+const ROCKET_VEL: f32 = 10.0;
 
 fn check_cursor_bound(actor: &mut Actor, x: f32, y: f32) -> bool {
     let screen_x = x / 2.0;
@@ -73,10 +123,8 @@ fn cursor_move(actor: &mut Actor, x: f32, y: f32, input: &InputState, dt: f32) {
     }
 }
 
-fn create_player_cursor() -> Actor {
-    Actor {
-        pos: Point2::ZERO,
-    }
+fn rocket_move(actor: &mut Actor, dt: f32) {
+    actor.pos += vec_from_angle(actor.angle) * ROCKET_VEL * dt;
 }
 
 struct MainState {
@@ -84,26 +132,34 @@ struct MainState {
     screen_width: f32,
     screen_height: f32,
     input: InputState,
+    rockets: Vec<Actor>,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let mut seed: [u8; 8] = [0; 8];
+        getrandom::getrandom(&mut seed[..]).expect("Could not create RNG seed");
+        let mut rng = Rand32::new(u64::from_ne_bytes(seed));
+
         let player = create_player_cursor();
 
         let (width, height) = ctx.gfx.drawable_size();
+
+        let rockets = create_rockets(&mut rng, 10, width, height);
 
         let s = MainState {
             player,
             screen_width: width,
             screen_height: height,
             input: InputState::default(),
+            rockets,
         };
 
         Ok(s)
     }
 }
 
-fn draw_actor(
+fn draw_cursor(
     canvas: &mut graphics::Canvas,
     actor: &Actor,
     world_coords: (f32, f32),
@@ -120,6 +176,20 @@ fn draw_actor(
         );
 }
 
+fn draw_rocket(canvas: &mut graphics::Canvas, actor: &Actor, world_coords: (f32, f32)) {
+    let (screen_w, screen_h) = world_coords;
+    let pos = world_to_screen_coords(screen_w, screen_h, actor.pos);
+    let rect = graphics::Rect::new(pos.x, pos.y, ROCKET_WIDTH, ROCKET_HEIGHT);
+    canvas.draw(
+        &graphics::Quad,
+        graphics::DrawParam::new()
+            .dest(rect.point())
+            .scale(rect.size())
+            .color(Color::WHITE),
+    );
+    let mb = &mut graphics::MeshBuilder::new();
+}
+
 impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         const DESIRED_FPS: u32 = 60;
@@ -128,6 +198,10 @@ impl EventHandler for MainState {
             let seconds = 1.0 / (DESIRED_FPS as f32);
 
             cursor_move(&mut self.player, self.screen_width, self.screen_height, &self.input, seconds);
+        
+            for rocket in &mut self.rockets {
+                rocket_move(rocket, seconds);
+            }
         }
 
         Ok(())
@@ -140,7 +214,11 @@ impl EventHandler for MainState {
             let coords = (self.screen_width, self.screen_height);
             let p = &self.player;
 
-            draw_actor(&mut canvas, p, coords);
+            draw_cursor(&mut canvas, p, coords);
+
+            for rocket in &self.rockets {
+                draw_rocket(&mut canvas, rocket, coords);
+            }
         }
 
         canvas.finish(ctx)?;
