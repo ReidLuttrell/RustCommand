808c5c04a490e3c648e239ab16730968ec36bc59
Reid Luttrell <lut@pdx.edu>
2023-11-16 16:58:36 -0800

    built framework and added a moveable cursor with bound checking

---
+++ RustCommand/src/main.rs
@@ -0,0 +1,215 @@
+use ggez::conf;
+use ggez::event::{self, EventHandler};
+use ggez::glam::*;
+use ggez::graphics::{self, Color};
+use ggez::input::keyboard::KeyCode;
+use ggez::timer;
+use ggez::{Context, ContextBuilder, GameResult};
+
+use ggez::input::keyboard::KeyInput;
+use std::env;
+use std::path;
+
+type Point2 = Vec2;
+
+fn world_to_screen_coords(screen_width: f32, screen_height: f32, point: Point2) -> Point2 {
+    let x = point.x + screen_width / 2.0;
+    let y = screen_height - (point.y + screen_height / 2.0);
+    Point2::new(x, y)
+}
+
+#[derive(Debug)]
+struct InputState {
+    xaxis: f32,
+    yaxis: f32,
+    fire: bool,
+}
+
+impl Default for InputState {
+    fn default() -> Self {
+        InputState {
+            xaxis: 0.0,
+            yaxis: 0.0,
+            fire: false,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Actor {
+    pos: Point2,
+}
+
+const CURSOR_VEL: f32 = 200.0;
+const CURSOR_WIDTH: f32 = 40.0;
+const CURSOR_HEIGHT: f32 = 15.0;
+
+fn check_cursor_bound(actor: &mut Actor, x: f32, y: f32) -> bool {
+    let screen_x = x / 2.0;
+    let screen_y = y / 2.0;
+
+    if actor.pos.x + CURSOR_WIDTH > screen_x {
+        // can't let the cursor get stuck, so adjust for each case
+        actor.pos -= Vec2::new(1.0, 0.0);
+        return false;
+    } else if actor.pos.x < -screen_x {
+        actor.pos += Vec2::new(1.0, 0.0);
+        return false;
+    }
+
+    if actor.pos.y > screen_y {
+        actor.pos -= Vec2::new(0.0, 1.0);
+        return false;
+    } else if actor.pos.y - CURSOR_HEIGHT < -screen_y {
+        actor.pos += Vec2::new(0.0, 1.0);
+        return false;
+    }
+    return true;
+}
+
+fn cursor_move(actor: &mut Actor, x: f32, y: f32, input: &InputState, dt: f32) {
+    if check_cursor_bound(actor, x, y) {
+        actor.pos += Vec2::new(input.xaxis * CURSOR_VEL * dt, input.yaxis * CURSOR_VEL * dt);
+    }
+}
+
+fn create_player_cursor() -> Actor {
+    Actor {
+        pos: Point2::ZERO,
+    }
+}
+
+struct MainState {
+    player: Actor,
+    screen_width: f32,
+    screen_height: f32,
+    input: InputState,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let player = create_player_cursor();
+
+        let (width, height) = ctx.gfx.drawable_size();
+
+        let s = MainState {
+            player,
+            screen_width: width,
+            screen_height: height,
+            input: InputState::default(),
+        };
+
+        Ok(s)
+    }
+}
+
+fn draw_actor(
+    canvas: &mut graphics::Canvas,
+    actor: &Actor,
+    world_coords: (f32, f32),
+) {
+    let (screen_w, screen_h) = world_coords;
+    let pos = world_to_screen_coords(screen_w, screen_h, actor.pos);
+    let rect = graphics::Rect::new(pos.x, pos.y, CURSOR_WIDTH, CURSOR_HEIGHT);
+        canvas.draw(
+            &graphics::Quad,
+            graphics::DrawParam::new()
+                .dest(rect.point())
+                .scale(rect.size())
+                .color(Color::WHITE),
+        );
+}
+
+impl EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        const DESIRED_FPS: u32 = 60;
+
+        while ctx.time.check_update_time(DESIRED_FPS) {
+            let seconds = 1.0 / (DESIRED_FPS as f32);
+
+            cursor_move(&mut self.player, self.screen_width, self.screen_height, &self.input, seconds);
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
+
+        {
+            let coords = (self.screen_width, self.screen_height);
+            let p = &self.player;
+
+            draw_actor(&mut canvas, p, coords);
+        }
+
+        canvas.finish(ctx)?;
+
+        timer::yield_now();
+        Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        input: KeyInput,
+        _repeated: bool,
+    ) -> GameResult {
+        match input.keycode {
+            Some(KeyCode::Up) => {
+                self.input.yaxis = 1.0;
+            }
+            Some(KeyCode::Down) => {
+                self.input.yaxis = -1.0;
+            }
+            Some(KeyCode::Left) => {
+                self.input.xaxis = -1.0;
+            }
+            Some(KeyCode::Right) => {
+                self.input.xaxis = 1.0;
+            }
+            Some(KeyCode::Space) => {
+                self.input.fire = true;
+            }
+            Some(KeyCode::Escape) => ctx.request_quit(),
+            _ => (),
+        }
+        Ok(())
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, input: KeyInput) -> GameResult {
+        match input.keycode {
+            Some(KeyCode::Up | KeyCode::Down) => {
+                self.input.yaxis = 0.0;
+            }
+            Some(KeyCode::Left | KeyCode::Right) => {
+                self.input.xaxis = 0.0;
+            }
+            Some(KeyCode::Space) => {
+                self.input.fire = false;
+            }
+            _ => (),
+        }
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        path::PathBuf::from("./resources")
+    };
+
+    let cb = ContextBuilder::new("rustcommand", "Reid Luttrell")
+        .window_setup(conf::WindowSetup::default().title("RustCommand"))
+        .window_mode(conf::WindowMode::default().dimensions(1280.0, 960.0))
+        .add_resource_path(resource_dir);
+
+    let (mut ctx, events_loop) = cb.build()?;
+
+    let game = MainState::new(&mut ctx)?;
+    event::run(ctx, events_loop, game)
+}   
